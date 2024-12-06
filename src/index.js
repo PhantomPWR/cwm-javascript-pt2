161a0f89051e504f49af4fd6a34dccd2b4466b3f
Jean de Villiers <jeandevilliers@me.com>
2024-12-06 17:04:46 +0000

    ES6 tooling

---
+++ src/index.js
@@ -0,0 +1,11 @@
+console.log('src/index.js');
+
+const Circle = require('./circle.js');
+
+const circle = new Circle(1);
+circle.draw(); // Circle with radius 1
+
+// Terminal command for running this file:
+// node es6-tooling/index.js
+
+console.log('changed...');
